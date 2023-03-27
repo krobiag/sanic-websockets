c6cd56329ebfaea690c77a0cba5e5c51cf660bf4
Kirk Robertson <kirk.robertson@iag.com.au>
2023-03-28 10:07:34 +1100

    - ping pong example - ui styles

--- backend/socket.py
+++ backend/socket.py
@@ -1,5 +1,6 @@
 import socketio
-
+import datetime
+import json
 sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins="*")
 
 
@@ -16,3 +17,14 @@ def disconnect(sid):
 @sio.event
 async def received_random_country(sid, data):
     print('message from received_random_country', sid, data)
+
+
+@sio.event
+async def ping_server(sid):
+    now = datetime.datetime.now()
+    dt_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    await sio.emit("pong_server", {
+        "type": "pong",
+        "message": "Pong",
+        "timestamp": dt_str,
+    })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import dayjs from "dayjs";
 import "./App.css";
 import useSocket from "./hooks/useSocket";
+interface IPingPongMessage {
+  type: "ping" | "pong";
+  message: string;
+  timestamp: string;
+}
 
 function App() {
   const [countries, setCountries] = useState<string[]>([]);
+  const [pingPongSever, setPingPongServer] = useState<IPingPongMessage[]>([]);
   const { socket, connected, disconnected, connecting } = useSocket();
 
   // Listen for the visit_random_country_generator message to emit from the server
@@ -13,29 +20,64 @@ function App() {
     socket.emit("received_random_country", data);
   });
 
+  // Receive Pong message from server
+  socket?.on("pong_server", (res) => {
+    setPingPongServer([...pingPongSever, res]);
+  });
+
   // Test socket disconnect/reconnect
   const toggleConnection = () => {
     if (socket) {
       connected ? socket?.close() : socket.connect();
     }
   };
+
+  const pingServer = () => {
+    socket?.emit("ping_server");
+    const now = dayjs().format("YYYY-MM-DD hh:mm:ss");
+    setPingPongServer([
+      ...pingPongSever,
+      {
+        type: "ping",
+        message: "Ping",
+        timestamp: now,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <div>Connecting: {`${connecting}`}</div>
-          <div>Connected: {`${connected}`}</div>
-          <div>Disconnected: {`${disconnected}`}</div>
+        <div className="websocket-wrapper">
+          <b>Socket status</b>
+          <pre className="code-format">
+            <div>Connecting: {`${connecting}`}</div>
+            <div>Connected: {`${connected}`}</div>
+            <div>Disconnected: {`${disconnected}`}</div>
+          </pre>
+          <div>
+            <button onClick={toggleConnection}>
+              {connected ? `Disconnect` : `Connect`}
+            </button>
+          </div>
         </div>
-        <div>
-          <button onClick={toggleConnection}>
-            {connected ? `Disconnect` : `Connect`}
-          </button>
-        </div>
-        <div>
-          {countries.map((country: string, countryIndex: number) => (
-            <div key={`country-${countryIndex}`}>{country}</div>
-          ))}
+        <div className="websocket-wrapper">
+          <div>
+            <b>Ping Pong messages</b>
+            <pre className="code-format">
+              {pingPongSever.map(
+                (pingPongData: IPingPongMessage, pingPongIndex: number) => (
+                  <div key={`ping-pong-${pingPongIndex}`}>
+                    <span className={pingPongData?.type}>
+                      {pingPongData?.type === "pong" ? "Server" : "Client"}
+                    </span>
+                    {` message: ${pingPongData?.message} @ ${pingPongData?.timestamp}`}
+                  </div>
+                )
+              )}
+            </pre>
+            <button onClick={pingServer}>Ping Server</button>
+          </div>
         </div>
       </header>
     </div>
